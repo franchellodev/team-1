9605f6c9ea3e47a4cb7c230297343b4879569a86
YagizGonen <yagizgonen97@gmail.com>
2023-01-23 16:04:46 +0300

    xddd

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $db = new PDO("mysql:host=localhost;dbname=team;charset=utf8", "root", "");
+    $db = new PDO("mysql:host=localhost;dbname=team;charset=utf8", "root", "root");
     /* $db = new PDO("mysql:host=sefatoprak.com;dbname=sefatopr_franchello;charset=utf8", "sefatopr_fruser",
     "~adK5^=+}vFs"); */
     //$db = new PDO("mysql:host=142.132.182.39;dbname=franchello_panel;charset=utf8", "franchello_panel", "mhR6HeiU");
