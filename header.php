7fde691bdad145417de5ceab002be9c15f00fb1e
Say Hello - Franchello <115899037+franchellodev@users.noreply.github.com>
2023-01-23 12:45:32 +0000

    test

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ include("db.php");
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     
-    <title>User Delete</title>
+    <title>User Deletes</title>
   </head>
   <body class="p-5" style="background: url(https://media.baamboozle.com/uploads/images/63823/1593533265_786163);background-repeat: no-repeat;
   background-size: 100%;">
