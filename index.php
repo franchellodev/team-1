ce89ac26849cde216f0b1a2905a79fd11f897f6d
Hümay <456789hz@gmail.com>
2023-01-25 10:25:04 +0300

    login

--- index.php
+++ index.php
@@ -10,15 +10,19 @@ $readUsers = $db->query("SELECT userId, userName, userSurname, userEmail FROM us
     <a href="insert.php" class="btn btn-primary float-right"><i class="fa fa-plus"></i> Veri Ekle</a>
   </div>
 </div>
-    <h3 class="text-center">Kullanıcıların VerileriX</h3>
+    <h3 class="text-center">Kullanıcıların Verileri 
+      <?php 
+      echo $_POST['name'];
+      ?>
+    </h3>
     <div class="table-responsive" style="background: #fff;">
     <table class="table table-bordered">
         <thead>
             <tr>
-                <td>Kullanıcı Adı</td>
-                <td>Kullanıcı Soyadı</td>
+                <td>Adı</td>
+                <td>Soyadı</td>
                 <td>E-Posta</td>
-                <td>Parola</td>
+                <td>Şifre</td>
                 <td>İşlem</td>
             </tr>
         </thead>
@@ -28,6 +32,7 @@ $readUsers = $db->query("SELECT userId, userName, userSurname, userEmail FROM us
                 <td><?= $user['userName'] ?></td>
                 <td><?= $user['userSurname'] ?></td>
                 <td><?= $user['userEmail'] ?></td>
+                <td><?= $user['userPassword']?></td>
                 <td><a href="edit.php?userEdit=<?= $user['userId'] ?>" class="btn btn-primary" title="Veri Güncelle"><i class="fa fa-edit"></i></a> <a href="functions.php?kullaniciSil=<?= $user['userId'] ?>" class="btn btn-danger" title="Veri Sil">Kullanıcı Sil</a> </td>
             </tr>
             <?php } ?>
