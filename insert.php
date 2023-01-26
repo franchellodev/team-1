e7b076a8c3310a9654dd7f499141ad68e7cf5c54
Hümay <456789hz@gmail.com>
2023-01-26 12:19:49 +0300

    bazı görsel değişiklikler yapıldı

--- insert.php
+++ insert.php
@@ -3,29 +3,33 @@
 include("header.php");
 
 ?>
-<h3 class="text-center">Yeni Kullanıcı Kaydı <i class="fa fa-user"></i></h3> 
+<h3 class="text-center"> Yeni Kullanıcı Kaydı <i class="fa fa-user"></i></h3> 
 <div class="row justify-content-center">
     
     <div class="col-md-6">
         <form action="functions.php" method="POST">
             <div class="form-group">
-                <label for="">Ad</label>
-                <input type="text" name="userName" class="form-control" required>
+                <label for="">Firma Adı</label>
+                <input type="text" name="Firma_Adi" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="">Soyad</label>
-                <input type="text" name="userSurname" class="form-control" required>
+                <label for="">Yönetici Ad-Soyad</label>
+                <input type="text" name="Yönetici_Ad-Soyad" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="">E-Posta</label>
-                <input type="text" name="userEmail" class="form-control" required>
+                <label for="">Telefon</label>
+                <input type="text" name="Telefon" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="">Parola</label>
-                <input type="password" name="userPassword" class="form-control" required>
+                <label for="">Mail</label>
+                <input type="text" name="Mail" class="form-control" required>
             </div>
             <div class="form-group">
-                <button type="submit" name="newUserForm" class="btn btn-primary text-center btn-block mt-5" value="1"><i class="fa fa-save"></i> Kullanıcı Kaydet</button>
+                <label for="">Adres</label>
+                <input type="text" name="Adres" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <button  type="submit" name="newUserForm" class="btn btn-primary text-center btn-block mt-5" value="1"><i class="fa fa-save"></i> Kullanıcıyı Kaydet </button>
                 <a href="index.php" class="btn btn-warning mt-3">Geri Dön</a>
             </div>
         </form>
